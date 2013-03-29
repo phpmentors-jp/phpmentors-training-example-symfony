d6f812246fcaa6a32a812e3d360cc47b03c38f8d
KUBO Atsuhiro <kubo@iteman.jp>
2013-03-29 14:49:56 +0900

    [Command] ユーザー登録テストコマンド app:test:user:registration を追加した。

---
+++ src/Example/UserRegistrationBundle/Command/TestUserRegistrationCommand.php
@@ -0,0 +1,88 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP version 5.3
+ *
+ * Copyright (c) 2012-2013 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    PHPMentors_Training_Example_Symfony
+ * @copyright  2012-2013 KUBO Atsuhiro <kubo@iteman.jp>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @since      File available since Release 1.0.0
+ */
+
+namespace Example\UserRegistrationBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Example\UserRegistrationBundle\Domain\Data\Factory\UserFactory;
+use Example\UserRegistrationBundle\Domain\Service\UserRegistrationService;
+
+/**
+ * @package    PHPMentors_Training_Example_Symfony
+ * @copyright  2012-2013 KUBO Atsuhiro <kubo@iteman.jp>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @since      Class available since Release 1.0.0
+ */
+class TestUserRegistrationCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('app:test:user:registration');
+        $this->setDescription('ユーザー登録テスト');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $userFactory = new UserFactory();
+        $user = $userFactory->create();
+        $user->setLastName('久保');
+        $user->setFirstName('敦啓');
+        $user->setEmail('foo@iteman.jp');
+        $user->setPassword('password');
+
+        $userRegistrationService = new UserRegistrationService(
+            $this->getContainer()->get('doctrine')->getEntityManager(),
+            $this->getContainer()->get('security.encoder_factory')->getEncoder($user),
+            $this->getContainer()->get('security.secure_random')
+        );
+        $userRegistrationService->register($user);
+
+        return 0;
+    }
+}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: utf-8
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
