67c87cb16ce4e58ac3ec8c03ee6d518412f3594a
YAMANE Nana <shigematsu.nana@gmail.com>
2014-11-22 20:51:15 +0900

    [Entity][Repository] User, UserRepository を追加した。
    User に EntityInterface、UserRepository に RepositoryInterface を実装した。

---
+++ src/Example/UserRegistrationBundle/Entity/User.php
@@ -0,0 +1,275 @@
+<?php
+/*
+ * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ *               2014 YAMANE Nana <shigematsu.nana@gmail.com>,
+ * All rights reserved.
+ *
+ * This file is part of PHPMentors_Training_Example_Symfony.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace Example\UserRegistrationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use PHPMentors\DomainKata\Entity\EntityInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="Example\UserRegistrationBundle\Repository\UserRepository")
+ * @ORM\Table(name="user",
+ *      uniqueConstraints={
+ *          @ORM\UniqueConstraint(name="user_email_idx", columns={"email"}),
+ *          @ORM\UniqueConstraint(name="user_activation_key_idx", columns={"activation_key"})
+ *      })
+ * @ORM\HasLifecycleCallbacks
+ */
+class User implements EntityInterface
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=255)
+     */
+    protected $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=255)
+     */
+    protected $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    protected $email;
+
+    /**
+     * @var string $password
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    protected $password;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="registration_date", type="datetime")
+     */
+    protected $registrationDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="activation_date", type="datetime", nullable=true)
+     */
+    protected $activationDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="activation_key", type="string", length=255)
+     */
+    protected $activationKey;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    protected $updatedAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param \DateTime $registrationDate
+     */
+    public function setRegistrationDate(\DateTime $registrationDate)
+    {
+        $this->registrationDate = $registrationDate;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getRegistrationDate()
+    {
+        return $this->registrationDate;
+    }
+
+    /**
+     * @param \DateTime $activationDate
+     */
+    public function setActivationDate(\DateTime $activationDate)
+    {
+        $this->activationDate = $activationDate;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getActivationDate()
+    {
+        return $this->activationDate;
+    }
+
+    /**
+     * @param string $activationKey
+     */
+    public function setActivationKey($activationKey)
+    {
+        $this->activationKey = $activationKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getActivationKey()
+    {
+        return $this->activationKey;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+}
---
+++ src/Example/UserRegistrationBundle/Repository/UserRepository.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ *               2014 YAMANE Nana <shigematsu.nana@gmail.com>,
+ * All rights reserved.
+ *
+ * This file is part of PHPMentors_Training_Example_Symfony.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace Example\UserRegistrationBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use PHPMentors\DomainKata\Entity\EntityInterface;
+use PHPMentors\DomainKata\Repository\RepositoryInterface;
+
+class UserRepository extends EntityRepository implements RepositoryInterface
+{
+    /**
+     * @param EntityInterface $entity
+     */
+    public function add(EntityInterface $entity)
+    {
+    }
+
+    /**
+     * @param EntityInterface $entity
+     */
+    public function remove(EntityInterface $entity)
+    {
+    }
+}
