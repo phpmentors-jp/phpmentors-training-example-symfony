9e1aace8323db22ec98e586e28e6c84c77f0f3c1
YAMANE Nana <shigematsu.nana@gmail.com>
2014-11-23 00:34:20 +0900

    Swift Mailer で ISO-2022-JP が使えるようにバンドルのブート時の設定を追加した。

--- src/Example/UserRegistrationBundle/ExampleUserRegistrationBundle.php
+++ src/Example/UserRegistrationBundle/ExampleUserRegistrationBundle.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of PHPMentors_Training_Example_Symfony.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
 
 namespace Example\UserRegistrationBundle;
 
@@ -6,4 +16,18 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ExampleUserRegistrationBundle extends Bundle
 {
+    public function boot()
+    {
+        $this->configureSwiftMailer();
+    }
+
+    protected function configureSwiftMailer()
+    {
+        \Swift::init(function () {
+            \Swift_DependencyContainer::getInstance()
+                ->register('mime.qpheaderencoder')
+                ->asAliasOf('mime.base64headerencoder');
+            \Swift_Preferences::getInstance()->setCharset('iso-2022-jp');
+        });
+    }
 }
