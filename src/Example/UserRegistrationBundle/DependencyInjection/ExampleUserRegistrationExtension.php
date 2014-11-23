e92a22e9c18e262c972a92a0152beb93608a1cc7
YAMANE Nana <shigematsu.nana@gmail.com>
2014-11-23 19:05:58 +0900

    [DependencyInjection] サービス定義ファイルのフォーマットを YAML に変更した。

--- src/Example/UserRegistrationBundle/DependencyInjection/ExampleUserRegistrationExtension.php
+++ src/Example/UserRegistrationBundle/DependencyInjection/ExampleUserRegistrationExtension.php
@@ -1,16 +1,51 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP version 5.3
+ *
+ * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    PHPMentors_Training_Example_Symfony
+ * @copyright  2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @license    http://opensource.org/licenses/BSD-2-Clause  The BSD 2-Clause License
+ * @since      File available since Release 1.0.0
+ */
 
 namespace Example\UserRegistrationBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * @package    PHPMentors_Training_Example_Symfony
+ * @copyright  2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @license    http://opensource.org/licenses/BSD-2-Clause  The BSD 2-Clause License
+ * @since      Class available since Release 1.0.0
  */
 class ExampleUserRegistrationExtension extends Extension
 {
@@ -22,7 +57,7 @@ class ExampleUserRegistrationExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
