bccb341a8b1a9b59600e6c364dc6c0f9915b1604
YAMANE Nana <shigematsu.nana@gmail.com>
2014-11-23 16:33:13 +0900

    [Controller] ユーザー登録のページフローを実装した。

--- src/Example/UserRegistrationBundle/Controller/UserRegistrationController.php
+++ src/Example/UserRegistrationBundle/Controller/UserRegistrationController.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2012-2013 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of PHPMentors_Training_Example_Symfony.
@@ -12,18 +12,60 @@
 
 namespace Example\UserRegistrationBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserRegistrationController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @var string
      */
-    public function indexAction($name)
+    private static $VIEW_INPUT = 'ExampleUserRegistrationBundle:UserRegistration:registration_input.html.twig';
+
+    /**
+     * @var string
+     */
+    private static $VIEW_CONFIRMATION = 'ExampleUserRegistrationBundle:UserRegistration:registration_confirmation.html.twig';
+
+    /**
+     * @var string
+     */
+    private static $VIEW_SUCCESS = 'ExampleUserRegistrationBundle:UserRegistration:registration_success.html.twig';
+
+    /**
+     * @return Response
+     *
+     * @Route("/users/registration/")
+     * @Method("GET")
+     */
+    public function inputAction()
     {
-        return array('name' => $name);
+        return $this->render(self::$VIEW_INPUT, array(
+            'form' => $this->createFormBuilder()->getForm()->createView(),
+        ));
+    }
+
+    /**
+     * @param  Request  $request
+     * @return Response
+     *
+     * @Route("/users/registration/")
+     * @Method("POST")
+     */
+    public function inputPostAction(Request $request)
+    {
+        $form = $this->createFormBuilder()->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            return $this->redirect($this->generateUrl('example_userregistration_userregistration_confirmation', array(), true));
+        } else {
+            return $this->render(self::$VIEW_INPUT, array(
+                'form' => $form->createView(),
+            ));
+        }
     }
 }
