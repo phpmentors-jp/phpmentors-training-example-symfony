5fca713b329709748c850370a211bf2a9400fcdb
YAMANE Nana <shigematsu.nana@gmail.com>
2014-11-23 16:25:13 +0900

    [Controller] PHPDoc を追加した。

--- src/Example/UserRegistrationBundle/Tests/Controller/UserRegistrationControllerTest.php
+++ src/Example/UserRegistrationBundle/Tests/Controller/UserRegistrationControllerTest.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of PHPMentors_Training_Example_Symfony.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
 
 namespace Example\UserRegistrationBundle\Tests\Controller;
 
