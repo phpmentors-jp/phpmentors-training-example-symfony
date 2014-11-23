44f8c1cd79384de6db809c1fefc1a46ab9127004
YAMANE Nana <shigematsu.nana@gmail.com>
2014-11-23 19:05:58 +0900

    [DependencyInjection] サービス定義ファイルのフォーマットを YAML に変更した。

--- src/Example/UserRegistrationBundle/DependencyInjection/ExampleUserRegistrationExtension.php
+++ src/Example/UserRegistrationBundle/DependencyInjection/ExampleUserRegistrationExtension.php
@@ -1,17 +1,22 @@
 <?php
+/*
+ * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of PHPMentors_Training_Example_Symfony.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
 
 namespace Example\UserRegistrationBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class ExampleUserRegistrationExtension extends Extension
 {
     /**
@@ -22,7 +27,7 @@ class ExampleUserRegistrationExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
