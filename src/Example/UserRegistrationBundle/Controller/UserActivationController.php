92576ec87cfa54ea641216b240acc015454e7c73
YAMANE Nana <shigematsu.nana@gmail.com>
2014-11-23 17:06:15 +0900

    [Controller] ユーザー有効化のページフローを実装した。

---
+++ src/Example/UserRegistrationBundle/Controller/UserActivationController.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ *               2014 YAMANE Nana <shigematsu.nana@gmail.com>,
+ * All rights reserved.
+ *
+ * This file is part of PHPMentors_Training_Example_Symfony.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace Example\UserRegistrationBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class UserActivationController extends Controller
+{
+    /**
+     * @var string
+     */
+    private static $VIEW_SUCCESS = 'ExampleUserRegistrationBundle:UserRegistration:activation_success.html.twig';
+
+    /**
+     * @param  Request               $request
+     * @return Response
+     * @throws NotFoundHttpException
+     *
+     * @Route("/users/registration/activation/")
+     * @Method("GET")
+     */
+    public function activationAction(Request $request)
+    {
+        if (!$request->query->has('key')) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render(self::$VIEW_SUCCESS);
+    }
+}
